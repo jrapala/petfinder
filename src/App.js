d42946c7d7e24da7f17acf8eee705fa4dbb8963d
Juliette Rapala <juliette@eventric.com>
2019-02-22 17:31:00 -0600

    Add CSS. Add NPM. Add linting, formatting, React, and Parcel.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-const Pet = (props) => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, props.name),
-		React.createElement('h2', {}, props.animal),
-		React.createElement('h2', {}, props.breed)
-	]);
-};
+import React from "react"
+import { render } from "react-dom"
 
-const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
-		React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
-		React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-	]);
-};
+const Pet = props => {
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, props.name),
+		React.createElement("h2", {}, props.animal),
+		React.createElement("h2", {}, props.breed),
+	])
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+// function component
+// const App = () => {
+// class component
+class App extends React.Component {
+	handleTitleClick() {
+		alert("you clicked the title")
+	}
+	render() {
+		// {} is for attributes
+		return React.createElement("div", {}, [
+			React.createElement(
+				"h1",
+				{ onClick: this.handleTitleClick },
+				"Adopt Me!"
+			),
+			// If you're creating a component, you pass in props instead of attributes
+			React.createElement(Pet, {
+				name: "Luna",
+				animal: "Dog",
+				breed: "Havanese",
+			}),
+			React.createElement(Pet, {
+				name: "Pepper",
+				animal: "Bird",
+				breed: "Cockatiel",
+			}),
+			React.createElement(Pet, {
+				name: "Doink",
+				animal: "Cat",
+				breed: "Mix",
+			}),
+		])
+	}
+}
+
+render(React.createElement(App), document.getElementById("root"))
